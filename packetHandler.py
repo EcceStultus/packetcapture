7dba56786d1fdc791badd77f3065623a62c208ae
mike <michaelkemq@gmail.com>
2025-02-02 19:59:29 +1300

    files

---
+++ packetHandler.py
@@ -0,0 +1,100 @@
+from scapy.all import *
+import logging
+import time
+import queue
+from datetime import datetime
+import threading
+import os
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(message)s")
+
+capture_directory = "captures"
+os.makedirs(capture_directory, exist_ok=True) # makes said Dir if user hasn't already created one
+
+save_interval = 300 # 5 minutes
+packet_queue = queue.Queue()
+stop_program = threading.Event()
+
+
+
+def log_ip_layer(pkt):
+    ip_layer = pkt["IP"]
+    logging.info(f"Src IP: {ip_layer.src}, Dst IP: {ip_layer.dst}, Protocol: {ip_layer.proto}")
+
+def log_tcp_layer(pkt):
+    tcp_layer = pkt["TCP"]
+    logging.info(f"TCP: Src Port: {tcp_layer.sport}, Dst Port: {tcp_layer.dport}, Flags: {tcp_layer.flags}, Window: {tcp_layer.window}")
+
+def log_udp_layer(pkt):
+    udp_layer = pkt["UDP"]
+    logging.info(f"UDP: Src Port: {udp_layer.sport}, Dst Port: {udp_layer.dport}")
+
+def log_dns_layer(pkt):
+    dns_layer = pkt["DNS"]
+    if dns_layer.qr == 0:
+        logging.info(f"DNS Query: {dns_layer.qd.qname.decode()}")
+
+def log_icmp_layer(pkt):
+    icmp_layer = pkt["ICMP"]
+    logging.info(f"ICMP Type: {icmp_layer.type}, Code: {icmp_layer.code}")
+
+def log_raw_layer(pkt):
+    raw_layer = pkt["RAW"].load
+    logging.info(f"Raw Payload: {raw_layer[:50]}")
+
+def pkt_handler(pkt):
+    if pkt.haslayer("IP"):
+        log_ip_layer(pkt)
+
+        if pkt["IP"].proto == 6:  # TCP
+            if pkt.haslayer("TCP"):
+                log_tcp_layer(pkt)
+                
+        elif pkt["IP"].proto == 17:  # UDP
+            if pkt.haslayer("UDP"):
+                log_udp_layer(pkt)
+
+    if pkt.haslayer("DNS"):
+        log_dns_layer(pkt)
+
+    if pkt.haslayer("ICMP"):
+        log_icmp_layer(pkt)
+
+    if pkt.haslayer("RAW"):
+        log_raw_layer(pkt)
+
+    packet_queue.put(pkt)
+
+def save_to_file():
+    time_stamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    file_name = os.path.join(capture_directory, f"{time_stamp}.pcap")
+
+    start_time = time.time()
+    packets = []
+
+    while not stop_program.is_set():    # runs until stop_program is called
+        try:
+            packet = packet_queue.get(timeout=1) # Gets packet from queue
+            packets.append(packet)
+        except queue.Empty:
+            continue    # If there is no packet, it tries again
+
+        if time.time() - start_time >= save_interval:   # Check if time limit has been surpassed
+            logging.info(f"Time interval surpassed, saving total {len(packets)} packets.")
+            wrpcap(file_name, packets)
+            packets = []
+            start_time = time.time()
+            time_stamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+            file_name = os.path.join(capture_directory, f"{time_stamp}.pcap")
+
+    if packets:
+        final_time_stamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        final_file_name = os.path.join(capture_directory, f"{final_time_stamp}_final.pcap")
+        wrpcap(final_file_name, packets)
+# TO-DO LIST
+#   - file compression
+#   - max file size limiter e.g either 5 minutes of 50Mb
+#   - 
+
+
+
---
+++ packetcaptureMAIN.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+from scapy.all import *
+import logging
+import packetHandler
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(message)s")
+
+def start_sniffer(interface):
+
+    try:
+        logging.info(f"Interface: {interface}")
+        logging.info("Starting packet capture: Ctrl+C to stop")
+
+        sniff(iface=interface, prn=packetHandler.pkt_handler, store=True)
+        
+    except KeyboardInterrupt:
+        logging.info("Program stopped by user")
+    except Exception as e:
+        logging.error(f"Error: {e}")
+
+def main():
+
+    interfaces = conf.ifaces
+    print("Interfaces Available: ")
+    print(interfaces)
+
+    interface = input("Select interface: ")
+
+    while interface not in interfaces:
+        print("Invalid interface")
+        interface = input("Select interface: ")
+
+    try:
+
+        packetHandler.save_thread = threading.Thread(target=packetHandler.save_to_file, daemon=True)
+        packetHandler.save_thread.start()
+
+        start_sniffer(interface)
+
+    finally:
+
+        packetHandler.stop_program.set()
+        packetHandler.save_thread.join()
+        logging.info("Final packets saved. Exiting")
+
+if __name__ == "__main__":
+    main()
